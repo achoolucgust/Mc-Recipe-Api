4a5cbbc7ae24c504c84ea85b0ef31cb0e36886cc
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2022-11-15 13:31:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from markupsafe import escape
 from flask import Response
+from flask import Flask
+import random
+import getdata
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return Response(status=200)
+def start():
+    f = open("./web/test.html")
+    e = f.read()
+    f.close()
+    return e
+@app.route('/recipe/<item>')
+def getrec(item):
+    try:
+        return getdata.recipes[item]
+    except:
+        return "invalid item"
+@app.route('/lang/<item>')
+def getname(item):
+    final = None
+    try:
+        final = getdata.lang["item.minecraft." + item]
+    except:
+        try:
+            final = getdata.lang["block.minecraft." + item]
+        except:
+            final = "invalid item"
+    return final
+@app.route('/splash')
+def splash():
+    f = open("./splash.txt")
+    r = f.read()
+    f.close()
+    return random.choice(r.split("\n"))
